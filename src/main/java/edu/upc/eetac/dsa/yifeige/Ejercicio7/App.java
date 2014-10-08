575187e4f153e21e20379830199b8dfa66a675b1
yifei ge <yokihappy@hotmail.com>
2014-10-08 18:52:15 +0200

    comentario añadido

--- src/main/java/edu/upc/eetac/dsa/yifeige/Ejercicio7/App.java
+++ src/main/java/edu/upc/eetac/dsa/yifeige/Ejercicio7/App.java
@@ -10,20 +10,20 @@ public class App
     	d.nombre="yifei";
     	d.edad=21;
     	d.sexo='M';
-    	Dato vacio=null;
+    	Dato vacio=null;// para injectar datos desde fichero de texto a este objeto vacio
     	try
     	{
-    		FileOutputStream fileOut=new FileOutputStream("datos.txt");
+    		FileOutputStream fileOut=new FileOutputStream("datos.txt");// construye un fichero de texto
     		ObjectOutputStream out=new ObjectOutputStream(fileOut);
-    		out.writeObject(d);
-    		out.close();
-    		fileOut.close();
+    		out.writeObject(d);// escribe mi objeto d(yifei,21,M) a este fichero
+    		out.close();//termino de escribir
+    		fileOut.close();//cierro el fichero
     		System.out.print("Los datos ha guardado en /home/yifeige/dsa-projects/Ejercicio7/datos.txt\n");
-    		FileInputStream filein=new FileInputStream("datos.txt");
+    		FileInputStream filein=new FileInputStream("datos.txt"); // leo el mismo fichero
     		ObjectInputStream in=new ObjectInputStream(filein);
-    		vacio=(Dato) in.readObject();
-    		in.close();
-    		filein.close();
+    		vacio=(Dato) in.readObject(); //los valores que leo lo injecto al objeto
+    		in.close(); //cierro el lector
+    		filein.close(); //cierro el fichero
     		
     		
     	}
@@ -31,7 +31,7 @@ public class App
     	{
     		e.printStackTrace();
     	}
-    	System.out.printf("El nombre es %s\n",vacio.nombre);
+    	System.out.printf("El nombre es %s\n",vacio.nombre); // muestra datos por la pantalla.
         System.out.printf("La edad es %d\n" + vacio.edad);
         System.out.printf("El sexo es %c\n" + vacio.sexo);
     	
